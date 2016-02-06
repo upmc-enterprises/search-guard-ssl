9984ca6bed9cff012bf265946d3ab23a60c7a3eb
floragunn <dev@search-guard.com>
2016-02-06 10:54:22 +0100

    fixed tests

--- src/test/java/org/elasticsearch/node/PluginAwareNode.java
+++ src/test/java/org/elasticsearch/node/PluginAwareNode.java
@@ -21,13 +21,12 @@ import java.util.Arrays;
 
 import org.elasticsearch.Version;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.internal.InternalSettingsPreparer;
 import org.elasticsearch.plugins.Plugin;
 
 public class PluginAwareNode extends Node {
 
     @SafeVarargs
     public PluginAwareNode(final Settings preparedSettings, final Class<? extends Plugin>... plugins) {
-        super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null), Version.CURRENT, Arrays.asList(plugins));
+        super(preparedSettings, Version.CURRENT, Arrays.asList(plugins));
     }
 }
